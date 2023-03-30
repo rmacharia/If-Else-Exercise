2208c5fabbcbacd07fe41f4e833858bf3fd246f5
rmacharia <r.githinji254@gmail.com>
2023-03-30 14:42:50 +0300

    Class

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int currentInventory = 10;
+int quantityToOrder = 5;
+
+if (currentInventory >= quantityToOrder)
+{
+    Console.WriteLine("Inventory is sufficient. No need to order more.");
+}
+else
+{
+    int quantityNeeded = quantityToOrder - currentInventory;
+    Console.WriteLine($"Inventory is low. Please order {quantityNeeded} more items.");
+}
